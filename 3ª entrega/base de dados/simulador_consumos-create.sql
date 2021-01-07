97232df61816bc06cf80d6bfabe8af4c7a70541c
Fredcostar <74367541+Fredcostar@users.noreply.github.com>
2021-01-07 14:37:44 +0000

    Add files via upload

--- 3ª entrega/base de dados/simulador_consumos-create.sql
+++ 3ª entrega/base de dados/simulador_consumos-create.sql
@@ -3,13 +3,13 @@
 create table utilizador (util_id int not null auto_increment,
 					 util_nome varchar(45) not null, #nome do utilizador
 					 util_pass varchar(45) not null, #paswword do utilizador
-					 primary key (uid));
+					 primary key (util_id));
                      
  create table redidencia (res_id int not null auto_increment,
 						res_morada varchar(45) not null, #morada da residencia
                         res_numero integer not null, #numero da residencia
                         res_tipo varchar(45) not null, #tipo de residencia
-                        primary key( reid));
+                        primary key( res_id));
                         
 create table atividades (ativ_id int not null auto_increment,
                      ativ_banho double not null,
@@ -17,7 +17,7 @@ create table atividades (ativ_id int not null auto_increment,
                      ativ_roupa double not null,
                      ativ_cozinhados double not null,
                      ativ_higiene double not null,
-                     primary key (aid));
+                     primary key (ativ_id));
                      
 create table resultados (resul_id int not null auto_increment,
                           resul_consumos varchar(45) not null, #consumo obtido 
@@ -25,28 +25,28 @@ create table resultados (resul_id int not null auto_increment,
                           resul_data varchar(45) not null, #data da simulação
                           resul_valorcalculado varchar(45), #valor calculado na simulação
                           resul_diferença varchar(45), #diferença entre valor obtido e valor de referencia
-                          primary key (rid));
+                          primary key (resul_id));
                           
  create table  registo (reg_id int not null auto_increment,
-                        primary key (rgid));
+                        primary key (reg_id));
                         
                         
                         
     # Chaves estrangeiras
 alter table registo add constraint utilizador_fk_registo
-            foreign key (utilizador_uid) references utilizador(util_id) 
+            foreign key (utilizador_util_id) references utilizador(util_id) 
 			ON DELETE NO ACTION ON UPDATE NO ACTION;    
             
 alter table registo add constraint residencia_fk_registo
-            foreign key (residencia_reid) references residencia(res_id) 
+            foreign key (residencia_res_id) references residencia(res_id) 
 			ON DELETE NO ACTION ON UPDATE NO ACTION; 
             
 alter table registo add constraint atividades_fk_registo
-            foreign key (atividades_aid) references atividades(ativ_id) 
+            foreign key (atividades_ativ_id) references atividades(ativ_id) 
 			ON DELETE NO ACTION ON UPDATE NO ACTION; 
             
 alter table resultados add constraint residencia_fk_resultados
-            foreign key (residencia_reid) references residencia(res_id) 
+            foreign key (residencia_res_id) references residencia(res_id) 
 			ON DELETE NO ACTION ON UPDATE NO ACTION; 
                           
                           
